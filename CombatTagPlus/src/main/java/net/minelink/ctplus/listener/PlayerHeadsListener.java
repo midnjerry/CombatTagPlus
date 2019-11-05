121f3a0b76a88fa28d6f6e08653ee3ecf4469388
Jerry Balderas <Gerardo.Balderas@cognizant.com>
2019-11-04 22:11:33 -0600

    Comment out unneeded dependencies - compile for 1.12.2

--- CombatTagPlus/src/main/java/net/minelink/ctplus/listener/PlayerHeadsListener.java
+++ CombatTagPlus/src/main/java/net/minelink/ctplus/listener/PlayerHeadsListener.java
@@ -9,7 +9,7 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
-import org.shininet.bukkit.playerheads.events.PlayerDropHeadEvent;
+//import org.shininet.bukkit.playerheads.events.PlayerDropHeadEvent;
 
 public final class PlayerHeadsListener implements Listener {
 
@@ -18,27 +18,27 @@ public final class PlayerHeadsListener implements Listener {
     public PlayerHeadsListener(CombatTagPlus plugin) {
         this.plugin = plugin;
     }
-
-    @EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
-    public void setOwner(PlayerDropHeadEvent event) {
-        Player player = event.getEntity();
-
-        // Do nothing if player is not a NPC
-        if (!plugin.getNpcPlayerHelper().isNpc(player)) return;
-
-        // Get NPC identity
-        NpcIdentity identity = plugin.getNpcPlayerHelper().getIdentity(player);
-        if (identity == null) return;
-
-        ItemStack item = event.getDrop();
-        item.setType(Material.SKULL_ITEM);
-
-        // Set skull owner to actual player's name
-        SkullMeta meta = (SkullMeta) item.getItemMeta();
-        meta.setOwner(identity.getName());
-
-        // Save meta
-        item.setItemMeta(meta);
-    }
+//
+//    @EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
+//    public void setOwner(PlayerDropHeadEvent event) {
+//        Player player = event.getEntity();
+//
+//        // Do nothing if player is not a NPC
+//        if (!plugin.getNpcPlayerHelper().isNpc(player)) return;
+//
+//        // Get NPC identity
+//        NpcIdentity identity = plugin.getNpcPlayerHelper().getIdentity(player);
+//        if (identity == null) return;
+//
+//        ItemStack item = event.getDrop();
+//        item.setType(Material.SKULL_ITEM);
+//
+//        // Set skull owner to actual player's name
+//        SkullMeta meta = (SkullMeta) item.getItemMeta();
+//        meta.setOwner(identity.getName());
+//
+//        // Save meta
+//        item.setItemMeta(meta);
+//    }
 
 }
--- CombatTagPlus/src/main/java/net/minelink/ctplus/util/BarUtils.java
+++ CombatTagPlus/src/main/java/net/minelink/ctplus/util/BarUtils.java
@@ -1,6 +1,6 @@
 package net.minelink.ctplus.util;
 
-import me.confuser.barapi.BarAPI;
+//import me.confuser.barapi.BarAPI;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,7 +16,7 @@ import org.bukkit.boss.BarStyle;
 import org.bukkit.boss.BossBar;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
-import org.inventivetalent.bossbar.BossBarAPI;
+//import org.inventivetalent.bossbar.BossBarAPI;
 
 public final class BarUtils {
 
@@ -28,11 +28,12 @@ public final class BarUtils {
             handler = Handler.NEW_BUKKIT_API;
         } else if (Bukkit.getPluginManager().isPluginEnabled("ActionBarAPI")) {
             handler = Handler.ACTIONBAR_API;
-        } else if (Bukkit.getPluginManager().isPluginEnabled("BarAPI")) {
-            handler = Handler.CONFUSER_BAR_API;
-        } else if (Bukkit.getPluginManager().isPluginEnabled("BossBarAPI")) {
-            handler = Handler.BOSSBAR_API;
         }
+//         else if (Bukkit.getPluginManager().isPluginEnabled("BarAPI")) {
+//            handler = Handler.CONFUSER_BAR_API;
+//        } else if (Bukkit.getPluginManager().isPluginEnabled("BossBarAPI")) {
+//            handler = Handler.BOSSBAR_API;
+//        }
     }
 
     public static boolean hasBar(Player player) {
@@ -122,51 +123,51 @@ public final class BarUtils {
             public void removeBar(Player player) {
 
             }
-        },
-
-        CONFUSER_BAR_API {
-            @Override
-            public boolean hasBar(Player player) {
-                return BarAPI.hasBar(player);
-            }
-
-            @Override
-            public void setMessage(Player player, String message, int timeout) {
-                BarAPI.setMessage(player, message, timeout);
-            }
-
-            @Override
-            public void setMessage(Player player, String message, float percent) {
-                BarAPI.setMessage(player, message, percent);
-            }
-
-            @Override
-            public void removeBar(Player player) {
-                BarAPI.removeBar(player);
-            }
-        },
-
-        BOSSBAR_API {
-            @Override
-            public boolean hasBar(Player player) {
-                return BossBarAPI.hasBar(player);
-            }
-
-            @Override
-            public void setMessage(Player player, String message, int timeout) {
-                BossBarAPI.setMessage(player, message, 100, timeout);
-            }
-
-            @Override
-            public void setMessage(Player player, String message, float percent) {
-                BossBarAPI.setMessage(player, message);
-                BossBarAPI.setHealth(player, percent);
-            }
-
-            @Override
-            public void removeBar(Player player) {
-                BossBarAPI.removeBar(player);
-            }
+//        },
+//
+//        CONFUSER_BAR_API {
+//            @Override
+//            public boolean hasBar(Player player) {
+//                return BarAPI.hasBar(player);
+//            }
+//
+//            @Override
+//            public void setMessage(Player player, String message, int timeout) {
+//                BarAPI.setMessage(player, message, timeout);
+//            }
+//
+//            @Override
+//            public void setMessage(Player player, String message, float percent) {
+//                BarAPI.setMessage(player, message, percent);
+//            }
+//
+//            @Override
+//            public void removeBar(Player player) {
+//                BarAPI.removeBar(player);
+//            }
+//        },
+//
+//        BOSSBAR_API {
+//            @Override
+//            public boolean hasBar(Player player) {
+//                return BossBarAPI.hasBar(player);
+//            }
+//
+//            @Override
+//            public void setMessage(Player player, String message, int timeout) {
+//                BossBarAPI.setMessage(player, message, 100, timeout);
+//            }
+//
+//            @Override
+//            public void setMessage(Player player, String message, float percent) {
+//                BossBarAPI.setMessage(player, message);
+//                BossBarAPI.setHealth(player, percent);
+//            }
+//
+//            @Override
+//            public void removeBar(Player player) {
+//                BossBarAPI.removeBar(player);
+//            }
         };
 
         public abstract boolean hasBar(Player player);
